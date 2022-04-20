ba086401e86ae65ae96f3f5af2f108e9d856757d
RadtkeN <raddnc@gmail.com>
2022-04-19 23:40:39 -0500

    tailwind changes

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -30,8 +30,8 @@ const Welcome = () => {
 
     return (
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                         Send Crypto <br /> across the world
                     </h1>
@@ -82,7 +82,7 @@ const Welcome = () => {
                                     Address
                                     {/* {shortenAddress(currentAccount)} */}
                                 </p>
-                                <p className="text-white font-light text-lg mt-1">
+                                <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
                                 </p>
                             </div>
