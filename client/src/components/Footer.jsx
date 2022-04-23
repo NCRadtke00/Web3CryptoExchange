1dc826452ca71c1f534dbf69aa0699f021104041
RadtkeN <raddnc@gmail.com>
2022-04-23 04:20:45 -0500

    working on footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import logo from '../../src/cexblklogo.png';
 const Footer = () => {
     return (
-        <h1>
-            Footer
-        </h1>
+        <div>
+            <div>
+                <div>
+                    <img src={logo} alt="logo" className="w-32" />
+                </div>
+                <div>
+                    <p>Market</p>
+                    <p>Market</p>
+                    <p>Market</p>
+                    <p>Market</p>
+                </div>
+            </div>
+            <div>
+                <p>questions comments concerns?</p>
+                <p>customerservice@cex.com</p>
+            </div>
+            <div>
+                <p>
+                    all rights reserved.
+                </p>
+            </div>
+        </div>
     )
 }
 
