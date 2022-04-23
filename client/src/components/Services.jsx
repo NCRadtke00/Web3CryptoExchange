0213a339c7a69bfe1f4cd4dcc741b135ea441f80
RadtkeN <raddnc@gmail.com>
2022-04-23 03:41:40 -0500

    added service cards to display services offered

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -3,6 +3,17 @@ import { BsShieldFillCheck } from "react-icons/bs";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
+const ServiceCard = ({ color, title, icon, subtitle }) => (
+    <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+        <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
+            {icon}
+        </div>
+        <div className="ml-5 flex flex-col flex-1">
+            <h3 className="mt-2 text-white text-lg">{title}</h3>
+            <p className="mt-1 text-white text-sm md:w-9/12"> {subtitle}</p>
+        </div>
+    </div>
+);
 
 const Services = () => {
     return (
@@ -19,7 +30,9 @@ const Services = () => {
                     </p>
                 </div>
                 <div className="flex-1 flex flex-col justify-start items-center">
-
+                    <ServiceCard />
+                    <ServiceCard />
+                    <ServiceCard />
                 </div>
             </div>
 
