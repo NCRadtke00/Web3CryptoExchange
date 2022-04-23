16ded9205745aecc890f0e2b8471a0a165644393
RadtkeN <raddnc@gmail.com>
2022-04-23 04:11:56 -0500

    finished services section, going to work on transactions section later

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -10,15 +10,16 @@ const ServiceCard = ({ color, title, icon, subtitle }) => (
         </div>
         <div className="ml-5 flex flex-col flex-1">
             <h3 className="mt-2 text-white text-lg">{title}</h3>
-            <p className="mt-1 text-white text-sm md:w-9/12"> {subtitle}</p>
+            <p className="mt-1 text-white text-sm md:w-9/12">
+                {subtitle}
+            </p>
         </div>
     </div>
 );
-
 const Services = () => {
     return (
         <div className="flex w-full justify-center items-center gradient-bg-services">
-            <div className="flex mf:flew-row flex-col items-center justify-between md:p-20 py-12 px-4">
+            <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
                 <div className="flex-1 flex flex-col justify-start items-start">
                     <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
                         Services that we offer
@@ -30,9 +31,24 @@ const Services = () => {
                     </p>
                 </div>
                 <div className="flex-1 flex flex-col justify-start items-center">
-                    <ServiceCard />
-                    <ServiceCard />
-                    <ServiceCard />
+                    <ServiceCard
+                        color="bg-[#2952E3]"
+                        title="Guaranteed Security"
+                        icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+                        subtitle="Security is guaranteed! Maintaining privacy and the quality of our product are two of our core values!"
+                    />
+                    <ServiceCard
+                        color="bg-[#8945F8]"
+                        title="The best exchange rates"
+                        icon={<BiSearchAlt fontSize={21} className="text-white" />}
+                        subtitle="We work to ensure that you get the best exchange rates on the web! We even match competition rates!"
+                    />
+                    <ServiceCard
+                        color="bg-[#F84550]"
+                        title="Lightening fast transactions"
+                        icon={<RiHeart2Fill fontSize={21} className="text-white" />}
+                        subtitle="Our transaction speeds are the fastest in the world! Dont believe us, try our competition!"
+                    />
                 </div>
             </div>
 
