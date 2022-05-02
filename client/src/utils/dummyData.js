7848f9a870e782309a3ede18a5c762fd3007abb5
RadtkeN <raddnc@gmail.com>
2022-05-02 00:14:17 -0500

    updated dummy data

--- client/src/utils/dummyData.js
+++ client/src/utils/dummyData.js
@@ -1,17 +1,17 @@
 export default [
     {
         id: 1,
-        url: "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284",
-        message: "",
+        url: "https://media.giphy.com/media/G8DNxveBS83mw/giphy.gif",
+        message: "I'm proud of you",
         timestamp: "4/20/2022, 4:45:21 PM",
         addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
-        amount: "0.01",
+        amount: "10.8",
         addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
     },
     {
         id: 2,
-        url: "https://media4.popsugar-assets.com/files/2013/11/07/832/n/1922398/eb7a69a76543358d_28.gif",
-        message: "",
+        url: "https://media.giphy.com/media/yN4DpWZ6DppfeB99la/giphy.gif",
+        message: "So fetch",
         timestamp: "4/20/2022, 4:33:21 PM",
         addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
         amount: "0.01",
@@ -19,8 +19,8 @@ export default [
     },
     {
         id: 3,
-        url: "https://acegif.com/wp-content/uploads/gif-shaking-head-38.gif",
-        message: "",
+        url: "https://media.giphy.com/media/Z91wshOLQ8wGQ/giphy.gif",
+        message: "Wild card",
         timestamp: "4/20/2022, 4:20:00 PM",
         addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
         amount: "0.01",
@@ -28,7 +28,7 @@ export default [
     },
     {
         id: 4,
-        url: "https://i.pinimg.com/originals/68/a0/9e/68a09e774e98242871c2db0f99307420.gif",
+        url: "https://media.giphy.com/media/9SIqyC1XMWS4TQCMne/giphy.gif",
         message: "",
         timestamp: "4/20/2022, 4:18:21 PM",
         addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
@@ -37,8 +37,8 @@ export default [
     },
     {
         id: 5,
-        url: "https://i.pinimg.com/originals/73/d3/a1/73d3a14d212314ab1f7268b71d639c15.gif",
-        message: "",
+        url: "https://media.giphy.com/media/xmqHmb0vRfGZq/giphy.gif",
+        message: "whaa",
         timestamp: "4/20/2022, 4:08:21 PM",
         addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
         amount: "0.01",
@@ -46,7 +46,7 @@ export default [
     },
     {
         id: 6,
-        url: "https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif",
+        url: "https://giphy.com/clips/Lydia-Getachew-cardi-b-cardib-thats-suspicious-5oYJvtkl5R13IikfvT",
         message: "",
         timestamp: "4/20/2022, 4:04:00 PM",
         addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
