05753e4330c948d0a99aa0d0e115d859adae20d0
RadtkeN <raddnc@gmail.com>
2022-05-01 23:33:08 -0500

    fixed typo, and updated darkmode config

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
             </div>
             <div className="flex justify-center items-center flex-col mt-5">
                 <p className="text-white text-sm text-center">questions comments concerns?</p>
-                <p className="text-white text-sm text-center">raddnx@gmail.com</p>
+                <p className="text-white text-sm text-center">raddnc@gmail.com</p>
             </div>
             <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
             <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
